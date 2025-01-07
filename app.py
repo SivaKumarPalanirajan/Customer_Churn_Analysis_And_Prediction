9ee45288648d6fea1a8cfe2e9b460ee8648e233c
= <=>
2025-01-07 17:28:02 +0530

    Added app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+from sklearn.compose import ColumnTransformer 
+import pickle 
+import streamlit as st 
+
+st.markdown("WELCOME TO THE Customer Churn predictor")
+st.info("""We can predict if a customer is likely to Churn or Stay""")
+st.info("ENTER DETAILS BELOW")
